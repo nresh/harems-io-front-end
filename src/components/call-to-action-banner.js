b5ac6a2c3b440d6196f907a0fcde2b9ad40dec56
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-07-19 01:19:38 -0700

    one more tiny tweak for top form

--- src/components/call-to-action-banner.js
+++ src/components/call-to-action-banner.js
@@ -1,13 +1,13 @@
 import { Col, Row } from "react-bootstrap"
 import React from "react"
 
-export default ({ id, className, left, right, text, cta, children }) => {
+export default ({ id, className, text, cta, children }) => {
   return (
     <Row id={id} className={`align-items-center justify-content-center w-100 ${className || ''}`}>
-      <Col className="my-4 text-center" lg={left || 8} xs="auto">
+      <Col className="my-4 text-center" lg={8} md={6} sm={6} xs={8}>
         {text}
       </Col>
-      <Col className="mb-4 mt-md-4 text-center" lg={right || 4} xs="auto">
+      <Col className="mb-4 mt-sm-4 text-center" lg={4} md={5} sm={5} xs={8}>
         {cta}
       </Col>
     </Row>
