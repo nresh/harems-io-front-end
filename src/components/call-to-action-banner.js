4600fe3f1926981ba9e3b6485f4625811fdcfeb7
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-07-19 01:45:19 -0700

    one more tweak :pray:

--- src/components/call-to-action-banner.js
+++ src/components/call-to-action-banner.js
@@ -3,11 +3,11 @@ import React from "react"
 
 export default ({ id, className, text, cta, children }) => {
   return (
-    <Row id={id} className={`align-items-center justify-content-center w-100 ${className || ''}`}>
-      <Col className="my-4 text-center" lg={8} md={6} sm={6} xs={8}>
+    <Row id={id} className={`align-items-center justify-content-center mx-auto w-100 ${className || ''}`}>
+      <Col className="my-4 text-center" lg={8} md={6} sm={6} xs={10}>
         {text}
       </Col>
-      <Col className="mb-4 mt-sm-4 text-center" lg={4} md={5} sm={5} xs={8}>
+      <Col className="mb-4 mt-sm-4 text-center" lg={4} md={5} sm={5} xs={10}>
         {cta}
       </Col>
     </Row>
