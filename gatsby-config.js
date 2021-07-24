62940e43dc5d56c286d4c460d5b4ef1ca1646efd
01101000011011110110010100001010 <dontrail@nullbash.com>
2021-07-23 20:14:59 -0400

    added googlel anaylitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,15 @@ const path = require(`path`)
          name: `images`,
          path: path.join(__dirname, `src`, `images`),
        },
+       resolve: `gatsby-plugin-google-gtag`,
+       options: {
+         // You can add multiple tracking ids and a pageview event will be fired for all of them.
+         trackingIds: [
+           "G-8MV4EPB9KJ", // Google Analytics / GA
+           "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+           "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+         ],
+       },
        // options: {
        //   path: `${__dirname}/gatsby-config.js`,
        // },
