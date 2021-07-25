a0284ddcdd8129c49c1730bc65fc63cd8548f725
01101000011011110110010100001010 <dontrail@nullbash.com>
2021-07-25 07:20:36 -0400

    Updated by bottom

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -30,7 +30,7 @@ export default () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
           <Nav id="home-nav">
-            <UpperLink href="/#buy-on-open-sea">Buy</UpperLink>
+            <UpperLink href="https://opensea.io/collection/haremsalphaog">Buy</UpperLink>
             <UpperLink href="/#roadmap">Roadmap</UpperLink>
             <UpperLink href="/#team">Team</UpperLink>
           </Nav>
