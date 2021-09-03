51389ddd9f07ad90a1b5be5dca7056534a3f03e7
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-09-03 01:22:03 -0700

    implement the writing of submitted emails to google doc and dynamic form visuals
    - store script url as constant
    - use GET request instead of POST and add on the email input to the request url
    - keep track of request progress with loading state variable
    - use loading state variable to show some dynamic content: upon submission, make button disabled and show 3 dots instead.  also show "submitting..." in pink below the input field

--- src/components/email-form.js
+++ src/components/email-form.js
@@ -2,21 +2,27 @@ import { Button, Col, Form, Row } from "react-bootstrap"
 import React from "react"
 import {useState} from 'react'
 
+const GOOGLE_SHEETS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz_RVnd3CADntin3A57uogEtYluv_TSgB5JTKkFzhCHW82VzT61DV0Ju8x4I0zdvbgZ/exec"
+
 const EmailForm = ({ id, hideLabel, children }) => {
   const [email, setEmail] = useState("")
+  const [loading, setLoading] = useState(false)
 
   const handleSubmit =  (event) => {
     event.preventDefault();
 
-    const url = "https://script.google.com/macros/s/AKfycbxXaWrB_suID2Cs3VoDwDR8hZPOkH6KO1qHEiO2rhyZcAACKe4tG7LIpXW96GAuj1D5/exec"
+    setLoading(true)
+    const emailUrlStr = `?email=${encodeURIComponent(email)}`
 
-    const resp = fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({email: email}),
-      }).then(res => { return res.json() })
-     .then(res => console.log(res))
-     .catch(err => console.log(err))
+    const resp = fetch(`${GOOGLE_SHEETS_SCRIPT_URL}${emailUrlStr}`, {
+      method: 'GET'
+    }).then(res => { return res.json() })
+      .then(res => { console.log(res); setLoading(false) })
+      .catch(err => console.log(err))
   }
+
+  const arrow = () => <><span>&rarr;</span></>
+  const dots = () => <><span className="brand-pink">. . .</span></>
 
   return (
     <Form id={id} onSubmit={handleSubmit}>
@@ -25,13 +31,15 @@ const EmailForm = ({ id, hideLabel, children }) => {
           hideLabel || <Form.Label>GET THE LATEST</Form.Label>
         }
         <Row className="pt-3">
-          <Col>
+          <Col className="text-start">
             <Form.Control className="d-inline-block w-75" type="email" placeholder="Email Address" onChange={e => setEmail(e.target.value)} />
-            <Button className="bg-dark p-2 w-30" variant="primary" type="submit">&rarr;</Button>
+            <Button className={(loading ? "disabled" : "") + " bg-dark p-2 w-30"} variant="primary" type="submit" disabled={!!loading}>
+              { loading ? dots() : arrow() }
+            </Button>
           </Col>
         </Row>
-        <Form.Text className="text-muted">
-          We keep your email private.
+        <Form.Text className={(loading ? 'brand-pink' : 'text-muted') + ' d-flex'}>
+          { loading ? "Submitting..." : "We keep your email private." }
         </Form.Text>
       </Form.Group>
     </Form>
